6cfec3a1602a38e920dc1835a030acede4cc6314
AelCee8103 <211837174+AelCantila@users.noreply.github.com>
2025-06-15 14:02:07 +0800

    css edit

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ portfolio/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+import React from "react";
+import "../index.css"; // Ensure this path is correct based on your project structure
+
+export const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* Theme Toggle */}
+      <ThemeToggle />
+      {/* Background Effects */}
+
+      {/* Nav Bar*/}
+
+      {/* main content*/}
+
+      {/* Footer*/}
+    </div>
+  );
+};
